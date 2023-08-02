bf3880ad1292bd2f456d3560a99def21e6562bae
Yurui-Feng <yurifeng@bu.edu>
2023-08-01 21:07:09 -0700

    add google login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+
 function Header() {
+  const handleLogin = () => {
+    window.open("http://localhost:3000/auth/google", "_self");
+  };
+
   return (
     <header>
       <h1>
         <StickyNote2Icon className="stickyNoteIcon" fontSize="large" /> Notes
         App
       </h1>
+      <div className="googleSignIn">
+        <button
+          class="btn btn-block btn-social btn-google"
+          onClick={handleLogin}
+        >
+          <i class="fab fa-google"></i>
+          Sign In
+        </button>
+      </div>
     </header>
   );
 }
