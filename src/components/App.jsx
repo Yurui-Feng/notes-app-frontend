74afd74bcff906d9a8f617cae7d190edaa08b026
Yurui-Feng <yurifeng@bu.edu>
2023-08-07 16:04:38 -0700

    add backend url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const backendUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : process.env.REACT_APP_BACKEND_URL;
+      : "http://notes-backend-lb-176294790.us-west-2.elb.amazonaws.com";
   const [notes, updateNotes] = useState([]);
   const [isLoggedIn, setLoggedIn] = useState(false); // Add this line
 
